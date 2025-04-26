414c0010e557fc155274c288a9a997f4f20d6ba8
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-04-26 01:05:15 -0400

    refactor: streamline accordion functionality and improve code consistency

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 const accordionElements = document.querySelectorAll(".accordion");
 
+const togglePanelVisibility = (panel) => {
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+};
+
+const handleAccordionClick = (accordion) => {
+  accordion.classList.toggle("active");
+
+  const panel = accordion.nextElementSibling;
+
+  if (panel) {
+    togglePanelVisibility(panel);
+  }
+};
+
 // Function to set initial state for the first accordion
 const setInitialAccordionState = () => {
   const firstAccordion = document.querySelector(".accordion.active");
+
   if (firstAccordion) {
     const panel = firstAccordion.nextElementSibling;
+
     if (panel) {
-      panel.style.transition = 'none';
+      panel.style.transition = "none";
       panel.style.maxHeight = panel.scrollHeight + "px";
       panel.offsetHeight;
-      panel.style.transition = '';
+      panel.style.transition = "";
     }
   }
-}
+};
 
-document.addEventListener('DOMContentLoaded', setInitialAccordionState);
+document.addEventListener("DOMContentLoaded", setInitialAccordionState);
 
 accordionElements.forEach((accordion) => {
   accordion.addEventListener("click", () => {
-
-    accordion.classList.toggle("active");
-    const panel = accordion.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+    handleAccordionClick(accordion);
   });
 });
