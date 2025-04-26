267b2bc5acfe03248f00bc983601e2f2e287ec08
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-04-26 00:52:45 -0400

    feat: set initial state for the first accordion and adjust panel transition for better UX

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const accordionElements = document.querySelectorAll(".accordion");
 
+// Function to set initial state for the first accordion
+const setInitialAccordionState = () => {
+  const firstAccordion = document.querySelector(".accordion.active");
+  if (firstAccordion) {
+    const panel = firstAccordion.nextElementSibling;
+    if (panel) {
+      panel.style.transition = 'none';
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.offsetHeight;
+      panel.style.transition = '';
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', setInitialAccordionState);
+
 accordionElements.forEach((accordion) => {
   accordion.addEventListener("click", () => {
+
     accordion.classList.toggle("active");
     const panel = accordion.nextElementSibling;
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
